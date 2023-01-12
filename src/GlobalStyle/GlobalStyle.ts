8b51fac2381c9e25a728059c7a404aaa9b336b30
muhammed mubarak <oyindamola850@gmail.com>
2023-01-12 17:54:30 +0100

    login page

--- src/GlobalStyle/GlobalStyle.ts
+++ src/GlobalStyle/GlobalStyle.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 *{
@@ -8,8 +8,59 @@ export const GlobalStyle = createGlobalStyle`
 
 body{
     font-family: 'Playfair Display', serif;
-    /* font-family: 'Bitter', serif; */
-    /*use this commented fonts for headers */
+    
 }
+`;
 
+export const AuthInputWrapper = styled.div`
+  margin: 20px 0;
+  width: 350px;
+  @media (max-width: 900px) {
+    width: 80%;
+  }
+`;
+
+export const SocialMediaAuthWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+export const AuthComponentTextWrapper = styled.div`
+  margin: 10px 0 30px 0;
+`;
+
+export const AuthLayout = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+export const LeftAuthComponent = styled.div`
+  flex: 60%;
+
+  @media (max-width: 900px) {
+    display: none;
+  }
+`;
+
+export const RightAuthComponent = styled.div`
+  flex: 40%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 10rem;
+  @media (max-width: 900px) {
+    flex: 100%;
+    margin-top: 10rem;
+    justify-content: center;
+  }
+`;
+
+export const AuthBgImg = styled.img`
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+  object-position: center;
 `;
---
+++ src/components/text/Text.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ITextProps {
+  children: React.ReactNode;
+}
+
+const Header = styled.h2`
+  font-family: "Bitter", serif;
+  font-size: 32px;
+  line-height: 24px;
+`;
+const Text = styled.p`
+  font-size: 18px;
+  line-height: 24px;
+`;
+
+const CustomHeader = ({ children }: ITextProps) => {
+  return <Header>{children}</Header>;
+};
+
+const CustomText = ({ children }: ITextProps) => {
+  return <Text>{children}</Text>;
+};
+
+export { CustomHeader, CustomText };
